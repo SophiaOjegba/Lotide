b2b9abf2554762b4c6fe39ea33d92dc3b30ef23b
sophia ojegba <roseysofia@yahoo.com>
2023-07-07 11:40:25 -0400

    updated files

---
+++ Lotide/test/assertArraysEqualTest.js
@@ -0,0 +1,13 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const  eqArrays = require('../eqArrays');
+
+
+
+
+
+//Test case
+
+(assertArraysEqual([1, 2, 3], [1, 2, 3])); // Pass
+(assertArraysEqual([1, 2, 3], [3, 2, 1])); // Fail
+(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // Pass
+(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // Fail
