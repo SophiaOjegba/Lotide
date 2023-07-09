5f09e579ef0fca1c22aa451e630cac60dff58d76
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 20:31:05 -0400

    Update letterPositions.js

--- Lotide/letterPositions.js
+++ Lotide/letterPositions.js
@@ -1,25 +1,6 @@
-const eqArrays = function(a, b) {
-  if (a.length !== b.length) {
-    return false; 
-  }
+const eqArrays = require ('./eqArrays')
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false; 
-    }
-  }
-    return true;
-  
-}
-
-const assertArraysEqual = function(array1, array2){
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed: [${array1}] === [${array2}] \u{1F600}`);
-  } 
-  else {
-    console.log(`Assertion Failed: [${array1}] !== [${array2}] \u{1F612}`);
-  }
-};
+const assertArraysEqual = require ('./assertArraysEqual')
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -38,3 +19,5 @@ const letterPositions = function(sentence) {
 
 //Test Code
 console.log(assertArraysEqual(letterPositions("hello").e, [1]));
+
+module.exports = letterPositions;
