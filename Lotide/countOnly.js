3a6631920b86ffb98a48e612484e5099ec3ed47a
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 20:25:59 -0400

    Update countOnly.js

--- Lotide/countOnly.js
+++ Lotide/countOnly.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected){
-    console.log(`Assertion Passed: ${actual} === ${expected} \u{1F600}`);
-  }
-  else{
-    console.log(`Assertion Failed: ${actual} !== ${expected} \u{1F612}`);
-  }
-};
 
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+const assertEqual = require('./assertEqual');
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
     for(const item of allItems){
@@ -28,25 +19,4 @@ const countOnly = function(allItems, itemsToCount) {
   
 }
 
-
-
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-console.log(result1);
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
