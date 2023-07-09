2e869cc8d9118eee6ba91ae6af8934b3e6d675f3
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 20:23:46 -0400

    Update assertObjectsEqual.js

--- Lotide/assertObjectsEqual.js
+++ Lotide/assertObjectsEqual.js
@@ -1,16 +1,5 @@
-const eqObjects = function(obj1, obj2) {
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false;
-  }
-  for (let key in obj1) {
-    if ((Array.isArray(obj1[key]) || Array.isArray(obj2[key])) && !eqArrays(obj1[key], obj2[key])) {
-      return false;
-    } else if (obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require("./eqObjects");
+
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -21,10 +10,5 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-// Test code
-const obj1 = { a: 1, b: 2 };
-const obj2 = { a: 1, b: 2 };
-const obj3 = { a: 2, b: 3 };
 
-assertObjectsEqual(obj1, obj2); // Assertion Passed
-assertObjectsEqual(obj1, obj3); // Assertion Failed
+module.exports = assertObjectsEqual;
