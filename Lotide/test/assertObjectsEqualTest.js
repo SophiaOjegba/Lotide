d671ecaafc409c935d61465e6f84fc7c07cb86fc
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 20:45:29 -0400

    Update assertObjectsEqualTest.js

--- Lotide/test/assertObjectsEqualTest.js
+++ Lotide/test/assertObjectsEqualTest.js
@@ -1,18 +1,18 @@
+const assert = require('chai').assert;
+const assertObjectsEqual = require('../assertObjectsEqual');
+const assert = require('chai').assert;
+const assertObjectsEqual = require('../assertObjectsEqual');
 
-const assertObjectsEqual = require("../assertObjectsEqual");
-const {assert} = require('chai').assert;
+describe('#assertObjectsEqual', () => {
+  it('should fail when objects are not equal', () => {
+    const obj1 = { a: 1, b: 2 };
+    const obj2 = { a: 1, b: 3 };
+    assert.strictEqual(assertObjectsEqual(obj1, obj2), undefined);
+  });
 
-
-// Test code
-const obj1 = { a: 1, b: 2 };
-const obj2 = { a: 1, b: 2 };
-const obj3 = { a: 2, b: 3 };
-
-assertObjectsEqual(obj1, obj2); // Assertion Passed
-assertObjectsEqual(obj1, obj3); // Assertion Failed
-
-describe("#asserObjectsEqual", ()=> {
-  it("should return object1 === object 2", ()=>{
-    assert.deepEqual(assertObjectsEqual(obj1, obj2),obj1 === obj2);
+  it('should pass when objects are equal', () => {
+    const obj1 = { a: 1, b: 2 };
+    const obj2 = { a: 1, b: 2 };
+    assert.deepEqual(assertObjectsEqual(obj1, obj2), undefined);
   });
 });
