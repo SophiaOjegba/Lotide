ed4ac11f6e44682738bc3148baaac7b05adda4ed
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 20:23:13 -0400

    Update assertEqual.js

--- Lotide/assertEqual.js
+++ Lotide/assertEqual.js
@@ -1,13 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected){
-    console.log("Assertion Passed:" + actual + ' === ' + expected + '\u{1F600}');
+    console.log(`Assertion Passed: ${actual} === ${expected} \u{1F600}`);
   }
   else{
-    console.log("Assertion Failed:" + actual + ' !== ' + expected + '\u{1F612}');
+    console.log(`Assertion Failed: ${actual} !== ${expected} \u{1F612}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
