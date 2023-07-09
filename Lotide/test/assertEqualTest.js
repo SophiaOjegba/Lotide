97bbf8c24e66f2cfbc7485f12832abb6081aeefc
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 22:10:59 -0400

    Update assertEqualTest.js

--- Lotide/test/assertEqualTest.js
+++ Lotide/test/assertEqualTest.js
@@ -1,5 +1,16 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+describe('#assertEqual', () => {
+  it('should pass when actual and expected values are equal', () => {
+    const actual = 5;
+    const expected = 5;
+    assert.strictEqual(assertEqual(actual, expected), undefined);
+  });
+
+  it('should fail when actual and expected values are not equal', () => {
+    const actual = 'Hello';
+    const expected = 'World';
+    assert.strictEqual(assertEqual(actual, expected), undefined);
+  });
+});
