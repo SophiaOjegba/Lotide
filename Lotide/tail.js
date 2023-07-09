f7fd4c83b709aa4dd4c7d88ac7c4d7421c2c0711
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 20:32:18 -0400

    Update tail.js

--- Lotide/tail.js
+++ Lotide/tail.js
@@ -1,25 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected){
-    console.log(`Assertion Passed: ${actual} === ${expected} \u{1F600}`);
-  }
-  else{
-    console.log(`Assertion Failed: ${actual} !== ${expected} \u{1F612}`);
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
+const assertEqual = require('./assertEqual');
 const tail = function(array) {
   if (array.length <= 1) {
     return [];
   }
   return array.slice(1);
 };
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const tailResult = tail(words);
-assertEqual(words.length, 3);
-console.log(tailResult);
+module.exports = tail;
