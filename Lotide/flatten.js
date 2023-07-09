6168ae6d2c61e25288850af3db10f73237d641a2
Sophia Ojegba <108946795+SophiaOjegba@users.noreply.github.com>
2023-07-08 20:30:07 -0400

    Update flatten.js

--- Lotide/flatten.js
+++ Lotide/flatten.js
@@ -1,25 +1,7 @@
-const assertArraysEqual = function(array1, array2){
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed: [${array1}] === [${array2}] \u{1F600}`);
-  } 
-  else {
-    console.log(`Assertion Failed: [${array1}] !== [${array2}] \u{1F612}`);
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
-const eqArrays = function(a, b) {
-  if (a.length !== b.length) {
-    return false; 
-  }
+const eqArrays = require ('./eqArrays')
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false; 
-    }
-  }
-    return true;
-  
-}
 const flatten = function(array) {
   let flattenedArray = [];
 const flattener = function(arrayflatten) {
@@ -36,3 +18,5 @@ return flattenedArray;
 };
 
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
+
+module.exports = flatten;
